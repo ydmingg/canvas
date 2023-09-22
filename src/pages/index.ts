becac4f7ecbe1c8a470d6a51e176de07443f4a37
xmyy <aaabbb@xm.funxdata.com>
2023-09-22 15:01:34 +0800

    update layout

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -22,15 +22,17 @@ const tpl = () => {
         let oCanvas = document.createElement("canvas");
         let oP = document.createElement("p");
         oDiv.className = `row rowcolumn align-center`
-        oCanvasDiv.className = `${el.name} flex1 w-100\%`
+        oCanvasDiv.className = `${el.name} flex1 w-100\% mb-12`
         oCanvas.className = `border-radius-xl box-shadow-all w-100\%`
-        oCanvas.width = 300
-        oCanvas.height = 240
         oP.textContent = `${el.name}`
         oApp.appendChild(oDiv)
         oDiv.appendChild(oCanvasDiv)
         oCanvasDiv.appendChild(oCanvas)
         oDiv.appendChild(oP)
+
+        // 继承父容器大小
+        oCanvas.width = oCanvasDiv.clientWidth
+        oCanvas.height = oCanvasDiv.clientHeight
     })
     
 }
